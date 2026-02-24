b8356ead485e04f1a5d9f46fa4b568e33292c791
Sriram Swain <sriramswain03@gmail.com>
2026-02-24 11:57:30 -0800

    first commit

---
+++ src/main/java/com/api/imageIngestion/ImageIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.api.imageIngestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageIngestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/imageIngestion/ImageIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.imageIngestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageIngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

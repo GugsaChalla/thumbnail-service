cf0dc8604493174c5a23f73a0bccb5967795b011
Gugsa Challa <gchalla8@gmail.com>
2026-02-24 12:58:52 -0800

    modify name of class

--- src/main/java/com/api/imageIngestion/controller/HealthController.java
+++ src/main/java/com/api/imageIngestion/controller/HealthController.java
@@ -2,7 +2,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StatusController {
+public class HealthController {
 
     @GetMapping("/ping")
     public String ping() {
